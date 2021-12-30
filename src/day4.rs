72d3f5bdabb42a38bf0ea7cb462d8109c9dc0fd5
Tim Peters <tim@catminion.net>
2021-12-30 13:30:07 +1030

    Parallelise day 4 search
    Slightly better that twice as fast.

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,6 @@
 use crate::Part;
+use std::sync::mpsc;
+use std::thread;
 
 pub fn run(input: &str, part: Part) -> String {
     let key = parse_input(input);
@@ -15,27 +17,65 @@ fn parse_input(input: &str) -> String {
     input.lines().next().unwrap().to_string()
 }
 
-fn search_hash(key: &str, test: impl Fn(md5::Digest) -> bool) -> usize {
-    let mut num = 0;
-    loop {
-        let digest = md5::compute(format!("{}{}", key, num).as_bytes());
-        if test(digest) {
-            return num;
-        }
-        num += 1;
+fn search_hash(key: &str, zeros: u32) -> u32 {
+    let mut handles = vec![];
+    // channel used for threads to tell main when they have a result
+    let (tx, rx) = mpsc::channel();
+    for i in 0..4 {
+        let tx = tx.clone();
+        let key = key.to_owned();
+        // channel used by main to tell threads a result has been found
+        let (tstop, rstop) = mpsc::channel();
+        handles.push((
+            thread::spawn(move || {
+                let mut best: u32 = u32::MAX;
+                let mut num = i;
+                while num < best {
+                    let digest = md5::compute(format!("{}{}", key, num).as_bytes());
+                    if match zeros {
+                        5 => digest[0] == 0 && digest[1] == 0 && digest[2] < 16,
+                        6 => digest[0] == 0 && digest[1] == 0 && digest[2] == 0,
+                        _ => unreachable!(),
+                    } {
+                        let _ = tx.send(num); // don't care if error
+                        return Some(num);
+                    }
+                    num += 4;
+                    if let Ok(n) = rstop.try_recv() {
+                        best = n;
+                    }
+                }
+                None
+            }),
+            tstop,
+        ));
     }
+
+    // wait until first result has come in
+    let candidate = rx.recv().unwrap();
+
+    // send this found number to all threads; they will stop as soon as their search couldn't
+    // possibly find anything better.
+    for h in &handles {
+        let _ = h.1.send(candidate); // don't care if error because thread has exited
+    }
+
+    // wait for all threads to finish what they're doing then return the lowest number found.
+    // there could be multiple matches if there are results nearby and the wrong number was found
+    // faster than the best one.
+    handles
+        .into_iter()
+        .filter_map(|jh| jh.0.join().unwrap())
+        .min()
+        .unwrap()
 }
 
-fn part1(key: &str) -> usize {
-    search_hash(key, |digest| {
-        digest[0] == 0 && digest[1] == 0 && digest[2] < 16
-    })
+fn part1(key: &str) -> u32 {
+    search_hash(key, 5)
 }
 
-fn part2(key: &str) -> usize {
-    search_hash(key, |digest| {
-        digest[0] == 0 && digest[1] == 0 && digest[2] == 0
-    })
+fn part2(key: &str) -> u32 {
+    search_hash(key, 6)
 }
 
 #[test]
