a429ceefd5c80b7e4b2a0955465bd11588e3c735
Tim Peters <tim@catminion.net>
2022-12-14 11:50:40 +1030

    Add day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -7,7 +7,7 @@ pub fn run(input: &str, part: Part) -> String {
         "{}",
         match part {
             Part::One => part1(&ingredients),
-            Part::Two => 0,
+            Part::Two => part2(&ingredients),
         }
     )
 }
@@ -28,6 +28,32 @@ fn part1(ingredients: &[Ingredient]) -> i32 {
                 let score = cookie_score(ingredients, &recipe);
                 if score > max {
                     max = score;
+                }
+            }
+        }
+    }
+    max
+}
+
+fn part2(ingredients: &[Ingredient]) -> i32 {
+    let len = ingredients.len();
+    let mut recipe: Vec<usize> = vec![0; len];
+    let mut max = 0;
+
+    // TODO: dynamic based on length of ingredients
+    for r0 in 0..=100 {
+        for r1 in 0..=(100 - r0) {
+            for r2 in 0..=(100 - (r0 + r1)) {
+                recipe[0] = r0;
+                recipe[1] = r1;
+                recipe[2] = r2;
+                recipe[3] = 100 - (r0 + r1 + r2);
+                let calories = cookie_calories(ingredients, &recipe);
+                if calories == 500 {
+                    let score = cookie_score(ingredients, &recipe);
+                    if score > max {
+                        max = score;
+                    }
                 }
             }
         }
@@ -57,6 +83,14 @@ fn cookie_score(ingredients: &[Ingredient], recipe: &[usize]) -> i32 {
         .map(|(i, v)| v.texture * recipe[i] as i32)
         .sum();
     positive(capacity) * positive(durability) * positive(flavor) * positive(texture)
+}
+
+fn cookie_calories(ingredients: &[Ingredient], recipe: &[usize]) -> i32 {
+    ingredients
+        .iter()
+        .enumerate()
+        .map(|(i, v)| v.calories * recipe[i] as i32)
+        .sum()
 }
 
 fn positive(i: i32) -> i32 {
@@ -111,4 +145,5 @@ Dummy2: capacity 0, durability 0, flavor 0, texture 0, calories 0
 ";
     let ingredients = parse_input(test_input);
     assert_eq!(62842880, part1(&ingredients));
+    assert_eq!(57600000, part2(&ingredients));
 }
