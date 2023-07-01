0d5369b8022eaefdd801e02c900c7c7d4d649711
Tim Peters <tim@catminion.net>
2023-07-01 16:38:23 +0930

    Add day 17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -1,29 +1,71 @@
+use std::collections::HashMap;
+
 use crate::Part;
 
 pub fn run(input: &str, part: Part) -> String {
     let input = parse_input(input);
+    let result = combinations(&input, 150);
     format!(
         "{}",
         match part {
-            Part::One => combinations(&input, 0, 150),
-            Part::Two => 0,
+            Part::One => result.total_combinations(),
+            Part::Two => result.min_combinations(),
         }
     )
 }
 
-fn combinations(input: &Vec<i32>, index: usize, target: i32) -> u32 {
+struct Stats(HashMap<u32, u32>);
+
+impl Stats {
+    fn new() -> Self {
+        Stats(HashMap::new())
+    }
+
+    fn increment(&mut self, key: u32) {
+        self.0.entry(key).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    fn total_combinations(&self) -> u32 {
+        self.0.values().sum()
+    }
+
+    fn min_containers(&self) -> u32 {
+        match self.0.keys().min() {
+            Some(key) => *key,
+            None => 0,
+        }
+    }
+
+    fn min_combinations(&self) -> u32 {
+        match self.0.get(&self.min_containers()) {
+            Some(value) => *value,
+            None => 0,
+        }
+    }
+}
+
+// Returns a HashMap of <number of containers, number of combinations>
+fn combinations(input: &Vec<i32>, target: i32) -> Stats {
+    let mut stats = Stats::new();
+    search_combinations(input, target, &mut stats, 0, 0);
+    stats
+}
+
+fn search_combinations(input: &Vec<i32>, target: i32, stats: &mut Stats, count: u32, index: usize) {
     if target == 0 {
-        return 1;
+        stats.increment(count);
+        return;
     }
     if target < 0 {
-        return 0;
+        return;
     }
     if index == input.len() {
-        return 0;
+        return;
     }
-    // combinations over the rest of the index that:
-    //  include this container + don't include this container
-    combinations(input, index + 1, target - input[index]) + combinations(input, index + 1, target)
+    // search rest of vector without including this container
+    search_combinations(input, target - input[index], stats, count + 1, index + 1);
+    // search rest of vector including this container
+    search_combinations(input, target, stats, count, index + 1);
 }
 
 fn parse_input(input: &str) -> Vec<i32> {
@@ -32,6 +74,8 @@ fn parse_input(input: &str) -> Vec<i32> {
 
 #[test]
 fn test() {
-    let test_input = vec![20, 15, 10, 5, 5];
-    assert_eq!(combinations(&test_input, 0, 25), 4);
+    let result = combinations(&parse_input("20\n15\n10\n5\n5"), 25);
+    assert_eq!(result.total_combinations(), 4);
+    assert_eq!(result.min_containers(), 2);
+    assert_eq!(result.min_combinations(), 3)
 }
