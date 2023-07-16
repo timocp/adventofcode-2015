febf53d137669acfbe6c179e175067486f18cfa9
Tim Peters <tim@catminion.net>
2023-07-16 17:13:51 +0930

    Add day 24 part 2

--- src/day24.rs
+++ src/day24.rs
@@ -4,30 +4,30 @@ pub fn run(input: &str, part: Part) -> String {
     let weights = parse_input(input);
     format!(
         "{}",
-        match part {
-            Part::One => find_group1(&weights).quantum_entanglement(&weights),
-            Part::Two => 0,
-        }
+        find_group1(
+            &weights,
+            match part {
+                Part::One => 3,
+                Part::Two => 4,
+            },
+        )
+        .quantum_entanglement(&weights)
     )
 }
 
 // A bitmap representing a group of packages
 // bit 0 = 0th package, bit 1 = 1st package, etc.
-// This only works as long as the input is <= 128 packages.
+// This only works as long as there are at least as many bits as packages
 #[derive(Debug, Clone)]
-struct Group(u128);
+struct Group(u64);
 
 impl Group {
-    fn new(value: u128) -> Self {
+    fn new(value: u64) -> Self {
         Group(value)
     }
 
     fn package_count(&self) -> u32 {
         self.0.count_ones()
-    }
-
-    fn overlaps(&self, other: &Group) -> bool {
-        (self.0 & other.0) != 0
     }
 
     fn weights(&self, weights: &[u32]) -> Vec<u32> {
@@ -55,8 +55,8 @@ fn parse_input(input: &str) -> Vec<u32> {
 }
 
 // group1 is the group of minimum size that has the smallest quantum entanglement
-fn find_group1(weights: &[u32]) -> Group {
-    let group_weight = weights.iter().sum::<u32>() / 3;
+fn find_group1(weights: &[u32], count: u32) -> Group {
+    let group_weight = weights.iter().sum::<u32>() / count;
 
     let groups = find_groups(weights, group_weight);
 
@@ -91,13 +91,24 @@ fn find_groups(weights: &[u32], group_weight: u32) -> Vec<Group> {
 #[test]
 fn test() {
     let weights = vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
+
     let groups = find_groups(&weights, 20);
     assert_eq!(25, groups.len());
     for group in groups {
-        // println!("group: {:?} = {:?}", group, group.weights(&weights));
         assert_eq!(20, group.weights(&weights).iter().sum::<u32>());
     }
-    let group = find_group1(&weights);
+
+    let group = find_group1(&weights, 3);
     assert_eq!(vec![9, 11], group.weights(&weights));
     assert_eq!(99, group.quantum_entanglement(&weights));
+
+    let groups = find_groups(&weights, 15);
+    assert_eq!(16, groups.len());
+    for group in groups {
+        assert_eq!(15, group.weights(&weights).iter().sum::<u32>());
+    }
+
+    let group = find_group1(&weights, 4);
+    assert_eq!(vec![4, 11], group.weights(&weights));
+    assert_eq!(44, group.quantum_entanglement(&weights));
 }
